9a44b1904f2209c052a4c906194f0294ea544bd3
Hammad Maqbool <hammad417@gmail.com>
2013-07-30 12:58:03 +0500

    Gallery added

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,7 +3,12 @@ class Upload < ActiveRecord::Base
   belongs_to :uploadable, :polymorphic => true
 
   attr_accessible :upload, :uploadable_id, :uploadable_type
-  has_attached_file :upload
+  has_attached_file :upload,
+                    :styles => {
+                        :thumb => "75x75#",
+                        :small => "150x150#",
+                        :medium => "500x500>"
+                    }
 
   include Rails.application.routes.url_helpers
 
@@ -12,8 +17,10 @@ class Upload < ActiveRecord::Base
       "name" => read_attribute(:upload_file_name),
       "size" => read_attribute(:upload_file_size),
       "url" => upload.url(:original),
+      "thumbnail_url" => upload.url(:thumb),
       "delete_url" => upload_path(self),
       "delete_type" => "DELETE" 
     }
   end
+
 end
