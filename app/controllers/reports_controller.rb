2ecc368213c4feb3de3ec331481e8269f194ee85
Hammad Maqbool <hammad417@gmail.com>
2013-08-05 07:38:53 +0500

    report form added

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,32 +8,23 @@ class ReportsController < ApplicationController
   def new
     @shop = Shop.find params[:shop_id]
     @report = Report.new
-    @products = []
-    @brands = []
-    count = 0
-    Product.all.each do |product|
-      @products[count] = product
-      product.brands.each do |brand|
-        @report.report_lines.build(:product_id => product.id,:brand_id => brand.id)
-        @brands[count] = brand
-        count = count + 1
-      end
+    unless params[:product_id].blank?
+      @product = Product.find params[:product_id]
+    else
+      @product = ProductCategory.find params[:product_category_id]
     end
   end
 
   def create
     @shop = Shop.find params[:shop_id]
-    @report = Report.new params[:report]
-    @report.user = current_user
-    @report.shop_id = params[:shop_id]
+    params[:report].each do |report|
+      @report = Report.new report.last
+      @report.user = current_user
+      @report.shop_id = params[:shop_id]
+      @report.save
+    end
     respond_to do |format|
-      if @report.save
-        format.html { redirect_to @shop, notice: 'Report was successfully submitted.' }
         format.json { render json: @shop, status: :created, location: @shop }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
     end
   end
 
