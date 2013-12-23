f60e24112bf1adb7e4972042085bde4339b5461f
Hammad Maqbool <hammad417@gmail.com>
2013-12-23 16:30:20 +0500

    validation added

--- app/models/shop_category.rb
+++ app/models/shop_category.rb
@@ -7,6 +7,6 @@ class ShopCategory < ActiveRecord::Base
   accepts_nested_attributes_for :avatar
 
   validates :name, :length => {:minimum => 3, :maximum => 20}, :presence => true 
-  #validates_presence_of :avatar, :message => "^please upload the marker pin"
+  validates_presence_of :avatar, :message => "^please upload the marker pin"
 
 end
