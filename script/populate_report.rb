6a4b10b0f7fb4d52844bbc8abe40c57b3adf31c7
phaedra <phaedra@phaedra.(none)>
2014-03-04 11:04:37 +0500

    Minor change in report_script file

--- script/populate_report.rb
+++ script/populate_report.rb
@@ -6,13 +6,12 @@ reports.collect do |key, report|
 	display_reports =  report.select{|a| a.report_type == "display"}
 	sale_reports    =  report.select{|a| a.report_type == "sales"}
 	corner_reports  = 	report.select{|a| a.report_type == "display_corner"}
-	
+		
 	corner_reports.each_with_index do |display_corner,index|
 		sale = sale_reports[index]
 		display = display_reports[index]
-		if sale.post_id.nil? and display.post_id? and display_corner.post_id?
-			post = Post.new :shop_id => display_corner.shop_id, :dealer_id => display_corner.shop.dealer.id, :product_category_id => display_corner.report_lines.first.id, :user_id => display_corner.user.id  
-			post.save
+		if sale.post_id.nil? and display.post_id.nil? and display_corner.post_id.nil?
+			post = Post.create(:shop_id => display_corner.shop_id, :dealer_id => display_corner.shop.dealer.id, :product_category_id => display_corner.report_lines.first.product_category_id, :user_id => display_corner.user.id )
 			sale.post_id = display.post_id = display_corner.post_id = post.id
 			sale.save
 			display.save
