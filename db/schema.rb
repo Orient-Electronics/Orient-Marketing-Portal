5944d4bc019ba46d90c47a47c7682cfefcf755ef
phaedra <phaedra@phaedra.(none)>
2014-04-16 13:39:32 +0500

    Announcement Feature added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140415064712) do
+ActiveRecord::Schema.define(:version => 20140416080307) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(:version => 20140415064712) do
     t.string   "activity_type"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+  end
+
+  create_table "announcements", :force => true do |t|
+    t.string   "announcement"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "avatars", :force => true do |t|
