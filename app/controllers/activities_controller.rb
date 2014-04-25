1de5a763815acb2ae0ba9ddaacf22c91afd0fdf3
phaedra <phaedra@phaedra.(none)>
2014-04-25 18:30:38 +0500

    User Profile Page completed

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,6 +2,7 @@ class ActivitiesController < ApplicationController
   def index
   	@user = User.find  params[:user_id]
     @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: params[:user_id], owner_type: "User")
+    @subscribers = @user.subscribers
   end
 
   def create_subscriber
