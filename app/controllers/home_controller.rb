7c01ecc9aaa035d6d5128b3b4e71702c152bc5cb
phaedra <phaedra@phaedra.(none)>
2014-04-28 12:31:59 +0500

    User Edit Profile Modified

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    @activities = current_user.subscribers.collect(&:activities).flatten.sort{|a, b| b[:created_at] <=> a[:created_at]}
+    @subscriber_activities = current_user.subscribers.collect(&:activities).flatten.sort{|a, b| b[:created_at] <=> a[:created_at]}
   end
 
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,8 +6,26 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def update
-  	super
-  	current_user.create_activity :update, :owner => current_user
+    
+    if params[:user][:password].blank?
+      params[:user].delete :current_password
+      params[:user].delete :password
+      params[:user].delete :password_confirmation
+      if current_user.update_attributes(params[:user]) 
+    	  current_user.create_activity :update, :owner => current_user
+        redirect_to '/'
+      else
+        redirect_to '/'
+      end
+    else
+      if current_user.update_with_password(params[:user]) 
+        current_user.create_activity :update, :owner => current_user
+        sign_in @user, :bypass => true
+        redirect_to '/'
+      else
+        redirect_to '/'
+      end
+    end    
   end
 
   def destroy
