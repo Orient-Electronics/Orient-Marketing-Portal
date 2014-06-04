8b812bb3090ec359bf7d0f5103f0b17e01be220f
Suneel Kumar <suneel.kumar@phaedrasolutions.com>
2014-06-04 12:21:21 +0500

    Task script modified

--- script/task.rb
+++ script/task.rb
@@ -1,7 +1,7 @@
 tasks = Task.all
 tasks.each do |task|
-	assign_user = User.where(assigned_to: task.assigned_to).first
+	assign_user = User.where(id: task.assigned_to).first
 	task.destroy  if assign_user.nil?
-	assigned_user = User.where(assigned_by: task.assigned_by).first
+	assigned_user = User.where(id: task.assigned_by).first
 	task.destroy  if assigned_user.nil?
 end
