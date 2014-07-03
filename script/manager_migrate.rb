1d162257b4ca41cb0cd2f1568a5ab9e307ec481c
Suneel Kumar <suneel.kumar@phaedrasolutions.com>
2014-07-03 11:25:59 +0500

    manager and dealer data migration script modified

--- script/manager_migrate.rb
+++ script/manager_migrate.rb
@@ -3,5 +3,5 @@ managers.each do |manager|
 	shop = Shop.where(id: manager.shop_id).first
 	people = shop.peoples.build(designation: "Manager", name: manager.name, date_of_birth: manager.dob, cell_number: manager.cell_number)
 	people.build_avatar
-	shop.save
+	people.save
 end
