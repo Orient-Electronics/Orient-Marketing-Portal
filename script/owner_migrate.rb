393a5988e82536768bd9f7276e9793e3a0c1884e
Suneel Kumar <suneel.kumar@phaedrasolutions.com>
2014-07-04 10:04:59 +0500

    Script Modified and People Role Added

--- script/owner_migrate.rb
+++ script/owner_migrate.rb
@@ -3,5 +3,5 @@ owners.each do |owner|
 	shop = Shop.where(id: owner.shop_id).first
 	people = shop.peoples.build(designation: "Owner", name: owner.name, date_of_birth: owner.dob, cell_number: owner.cell_number)
 	people.build_avatar
-	people.save
+	shop.save
 end
